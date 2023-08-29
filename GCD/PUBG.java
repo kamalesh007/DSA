acb04ddc317dc3fe337d7a62e82cca5a62c264e8
kamalesh007 <42745580+kamalesh007@users.noreply.github.com>
2023-08-29 18:32:58 +0530

    Create PUBG.java

---
+++ GCD/PUBG.java
@@ -0,0 +1,20 @@
+// There are N players, each with strength A[i]. when player i attack player j, player j strength reduces to max(0, A[j]-A[i]). When a player's strength reaches zero, it loses the game, and the game continues in the same manner among other players until only 1 survivor remains.
+
+// Can you tell the minimum health last surviving person can have?
+
+public class Solution {
+    public int solve(int[] A) {
+    
+        int d=A[0];
+        for(int i=1;i<A.length;i++)
+        {
+            d=gcd(d,A[i]);
+        }
+        return d;
+    }
+    public int gcd(int A,int B)
+    {
+        if(B==0)return A;
+        return gcd(B,A%B);
+    }
+}
